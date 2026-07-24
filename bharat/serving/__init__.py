86aef4c5a7ecef8f7388dff7d776ecb0ac24891f
GenixBit Labs Private Limited <manojnitbhu2@gmail.com>
2026-07-24 10:53:40 +0530

    feat: export local writer contracts

--- bharat/serving/__init__.py
+++ bharat/serving/__init__.py
@@ -3,6 +3,12 @@ from __future__ import annotations
 from bharat.serving.auth import ApiKeyAuthenticator, AuthConfig, AuthResult
 from bharat.serving.controller import ServingController
 from bharat.serving.export import ExportFormat, ExportPlan, ExportRequest, build_export_plan
+from bharat.serving.export_writer import (
+    DryRunExportWriter,
+    ExportWriteResult,
+    ExportWriter,
+    ExportWriterRegistry,
+)
 from bharat.serving.metrics import MetricsSnapshot, ServingMetrics
 from bharat.serving.rate_limit import InMemoryRateLimiter, RateLimitConfig, RateLimitResult
 from bharat.serving.streaming import (
@@ -19,9 +25,13 @@ __all__ = [
     "ApiKeyAuthenticator",
     "AuthConfig",
     "AuthResult",
+    "DryRunExportWriter",
     "ExportFormat",
     "ExportPlan",
     "ExportRequest",
+    "ExportWriteResult",
+    "ExportWriter",
+    "ExportWriterRegistry",
     "FunctionCall",
     "FunctionSpec",
     "InMemoryRateLimiter",
